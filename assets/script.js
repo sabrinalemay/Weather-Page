f628c4e0b3e42922a59a4709d719a8cd822c9e69
Sabrina LeMay <b33na1997@gmail.com>
2022-09-30 22:23:52 -0700

    update to script

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,13 @@ var cityName = "";
 var temperature = document.querySelector('.temperature');
 var humidity = document.querySelector('.humidity');
 var currentDate = moment().format("M/D/YYYY");
-var forcast = [$("#dayOne"),$("#dayTwo"), $("#dayThree"), $("dayFour"), $("dayFive")];
+var forcast = [$("#dayOne"),$("#dayTwo"), $("#dayThree"), $("#dayFour"), $("#dayFive")];
 var wind = document.querySelector(".wind");
 
 searchBtn.addEventListener('click', getWeather)
 
 function getWeather(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=c7dcfbac4d6d7f4bb9b41c18434427cb')
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=c7dcfbac4d6d7f4bb9b41c18434427cb')
     .then(response => response.json())
     .then(data => {
         console.log(data)
@@ -32,11 +32,15 @@ function getWeather(){
         // uvIndex.text(data.)
 
        for(var i = 0; i < forcast.length; i++){
-           forcast[i].find(".temperature").text(data.weather[i+1].main.temp);
-           forcast[i].find(".humidity").text(data.weather[i+1].main.humidity);
-           forcast[i].find(".wind").text(data.weather[i+1].wind.speed);
+           forcast[i].find(".temperature").text(data.weather[i].main.temp);
+           forcast[i].find(".humidity").text(data.weather[i].main.humidity);
+           forcast[i].find(".wind").text(data.weather[i].wind.speed);
        }
     })
 };
+
+function weatherForcast() {
+    fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c7dcfbac4d6d7f4bb9b41c18434427cb')
+}
 
 
